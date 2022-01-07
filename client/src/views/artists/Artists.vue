d219905c54fa0e1a653099ac935b41d199ce7e73
ivankikic <ivankikic49@gmail.com>
2022-01-07 19:10:13 +0100

    Test

--- client/src/views/artists/Artists.vue
+++ client/src/views/artists/Artists.vue
@@ -38,7 +38,7 @@
 </template>
 
 <script>
-/*import axios from 'axios';
+import axios from 'axios';
 
 const config = {
     headers: {  Authorization: `bearer s%3ARhGn7I5uksizD9rqFitYgSGjlMvwc-NZ.iiE25xTPUIccUgHIgyoDLYdnuXpwT0MT4WGHkeWw5uo`,  
@@ -48,32 +48,35 @@ const config = {
 };
 const bodyParameters = {
    key: "value"
-};*/
+};
 export default {
     data() {
         return {
             artists: [
-                {"_id":"61cb0dfb5e9aec0934842fc6","idArtist":112509,"strArtist":"3 Doors Down","idLabel":47129,"intFormedYear":1996,"intBornYear":null,"strStyle":"Rock/Pop","strGenre":"Alternative Rock","strMood":"Sentimental","strWebsite":"www.3doorsdown.com","strFacebook":"www.facebook.com/3DoorsDown","strTwitter":"twitter.com/3doorsdown","strBiographyEN":"3 Doors Down is an American rock band from Escatawpa, Mississippi formed in 1996. Since 1998 the band consists of lead vocalist Brad Arnold, lead guitarist Chet Roberts, bass guitarist Todd Harrell and rhythm guitarist Chris Henderson. Their current drummer and percussionist is Greg Upchurch who has been with the band since 2005.\n\nThe band rose to international fame with their first single, \"Kryptonite\", which charted in the top three on the Billboard Hot 100 chart. The band then signed onto Republic Records and released their debut album, The Better Life, in 2000. The album was the 11th-best-selling album of the year and was certified 6x platinum in the United States. Their second album, Away from the Sun, (2002) continued the band's success; it debuted at #8 on the Billboard 200 chart and went multi-platinum in the United States like its predecessor. The band followed it up by extensive touring for two years before releasing their third album, Seventeen Days, in 2005. The album debuted at #1 on the Billboard 200 chart and was certified platinum within its first month of release. Their fourth, self-titled album, 3 Doors Down, also debuted at #1 on the Billboard 200 chart. The band's fifth studio album, Time of My Life, was released on July 19, 2011.\n\nThe band often performs more than 300 concerts a year and has shared the stage with artists such as Lynyrd Skynyrd, Megadeth, Staind, Nickelback, Alter Bridge, Breaking Benjamin, Seether, Shinedown, Hinder, Mentors, ZZ Top, and Daughtry. Since the start of their career, 3 Doors Down have sold over 20 million albums worldwide.","strCountry":"Escatawpa, Mississippi, USA","strArtistThumb":"https://www.theaudiodb.com/images/media/artist/thumb/qtuqrv1361747906.jpg","intListeners":2709577,"intPlaycount":63081766,"__v":0},
+                /*{"_id":"61cb0dfb5e9aec0934842fc6","idArtist":112509,"strArtist":"3 Doors Down","idLabel":47129,"intFormedYear":1996,"intBornYear":null,"strStyle":"Rock/Pop","strGenre":"Alternative Rock","strMood":"Sentimental","strWebsite":"www.3doorsdown.com","strFacebook":"www.facebook.com/3DoorsDown","strTwitter":"twitter.com/3doorsdown","strBiographyEN":"3 Doors Down is an American rock band from Escatawpa, Mississippi formed in 1996. Since 1998 the band consists of lead vocalist Brad Arnold, lead guitarist Chet Roberts, bass guitarist Todd Harrell and rhythm guitarist Chris Henderson. Their current drummer and percussionist is Greg Upchurch who has been with the band since 2005.\n\nThe band rose to international fame with their first single, \"Kryptonite\", which charted in the top three on the Billboard Hot 100 chart. The band then signed onto Republic Records and released their debut album, The Better Life, in 2000. The album was the 11th-best-selling album of the year and was certified 6x platinum in the United States. Their second album, Away from the Sun, (2002) continued the band's success; it debuted at #8 on the Billboard 200 chart and went multi-platinum in the United States like its predecessor. The band followed it up by extensive touring for two years before releasing their third album, Seventeen Days, in 2005. The album debuted at #1 on the Billboard 200 chart and was certified platinum within its first month of release. Their fourth, self-titled album, 3 Doors Down, also debuted at #1 on the Billboard 200 chart. The band's fifth studio album, Time of My Life, was released on July 19, 2011.\n\nThe band often performs more than 300 concerts a year and has shared the stage with artists such as Lynyrd Skynyrd, Megadeth, Staind, Nickelback, Alter Bridge, Breaking Benjamin, Seether, Shinedown, Hinder, Mentors, ZZ Top, and Daughtry. Since the start of their career, 3 Doors Down have sold over 20 million albums worldwide.","strCountry":"Escatawpa, Mississippi, USA","strArtistThumb":"https://www.theaudiodb.com/images/media/artist/thumb/qtuqrv1361747906.jpg","intListeners":2709577,"intPlaycount":63081766,"__v":0},
                 {"_id":"61cb0dfd5e9aec0934842fc8","idArtist":112055,"strArtist":"30 Seconds to Mars","idLabel":null,"intFormedYear":1998,"intBornYear":null,"strStyle":"Rock/Pop","strGenre":"Alternative Rock","strMood":"Epic","strWebsite":"thirtysecondstomars.thisisthehive.net","strFacebook":"www.facebook.com/thirtysecondstomars","strTwitter":"twitter.com/#!/30Secondstomars","strBiographyEN":"30 Seconds to Mars is an American rock band from Los Angeles, formed in 1998. Since 2007, the band has consisted of actor Jared Leto (lead vocals, rhythm guitar, songwriter), Shannon Leto (drums, percussion) and Tomo Tomo Miličević (lead guitar, keyboards). Following the departure of Matt Wachter (now of Angels & Airwaves) in 2007, Tim Kelleher became the bassist for the group, performing live only with both Jared and Tomo Miličević recording bass for studio recordings, while Braxton Olita (keyboards) was added to the touring lineup in 2009. Previously, the group also featured guitarists Solon Bixler (now of Great Northern) and Kevin Drake.\n\nTo date, 30 Seconds to Mars have released three studio albums - 30 Seconds to Mars (2002), A Beautiful Lie (2005), This Is War (2009) and three extended plays - AOL Sessions Undercover (2007), To the Edge of the Earth (2008), and MTV Unplugged: 30 Seconds to Mars (2011).\n\n30 Seconds to Mars' music has been associated mostly with progressive rock, progressive metal and post-grunge, but they have also included screamo, emo, space rock, synth rock and post-hardcore into their music. They have been compared to Pink Floyd (who were an influence on their work) and Tool because of their use of philosophical and spiritual lyrics, concept albums and their use of experimental music.","strCountry":"Los Angeles, CA","strArtistThumb":"https://www.theaudiodb.com/images/media/artist/thumb/30-seconds-to-mars-4fb1681b602b8.jpg","intListeners":2427465,"intPlaycount":118342981,"__v":0},
                 {"_id":"61cb0dff5e9aec0934842fca","idArtist":111985,"strArtist":"50 Cent","idLabel":44378,"intFormedYear":1996,"intBornYear":1975,"strStyle":"Urban/R&B","strGenre":"Hip-Hop","strMood":"Angry","strWebsite":"50cent.com","strFacebook":"www.facebook.com/50cent","strTwitter":"twitter.com/50cent","strBiographyEN":"Curtis James Jackson III (born July 6, 1975), better known by his stage name 50 Cent, is an American rapper, actor, entrepreneur, investor, record, film, and television producer. Born in the South Jamaica neighborhood of the borough of Queens, Jackson began selling drugs at age twelve during the 1980s crack epidemic. Although he left drug-dealing to pursue a musical career, he was struck by nine bullets in a 2000 shooting. After Jackson released the compilation album Guess Who's Back? in 2002, he was discovered by Eminem and signed by Shady Records, Aftermath Entertainment and Interscope Records.\nWith the aid of Eminem and Dr. Dre (who produced his first major-label album, Get Rich or Die Tryin'), Jackson became one of the world's best selling rappers and rose to prominence with East Coast hip hop group G-Unit (which he leads de facto). In 2003 he founded G-Unit Records, signing his G-Unit associates Young Buck, Lloyd Banks and Tony Yayo. Jackson had similar commercial and critical success with his second album, The Massacre, which was released in 2005. He released his fifth studio album, Animal Ambition, in 2014 and is working on his sixth studio album: Street King Immortal, scheduled for release in 2016.\nDuring his career Jackson has sold over 30 million albums worldwide and won several awards, including a Grammy Award, thirteen Billboard Music Awards, six World Music Awards, three American Music Awards and four BET Awards. He has pursued an acting career, appearing in the semi-autobiographical film Get Rich or Die Tryin' (2005), the Iraq War film Home of the Brave (2006) and Righteous Kill (2008). 50 Cent was ranked the sixth-best artist of the 2000s, the third-best rapper (behind Eminem and Nelly), and Get Rich or Die Tryin' and The Massacre were ranked the 12th and 37th best albums of the decade by Billboard.","strCountry":"New York, USA","strArtistThumb":"https://www.theaudiodb.com/images/media/artist/thumb/ya6y871615076491.jpg","intListeners":3033749,"intPlaycount":48992348,"__v":0}
-            ],
+            */],
         }
     },
+    mounted() {
         /*axios.get('http://localhost:3000/auth/facebook', 
         bodyParameters, 
         { headers: { Accept: "application/json",
                             "Content-Type": "application/json",
                             "Access-Control-Allow-Credentials": true }}, 
         {credentials: 'include'}).then(response => console.log(response.data));*/
-        /*axios.get(
-        'http://localhost:3000/auth/facebook',
+        axios.get(
+        'http://localhost:3000/artist',
         {
             credentials: 'include'
         },
         config,
-        ).then(response => console.log(response.data))*/
-    }
+        ).then(response => console.log(response.data))
+    }, 
 
+
+}
 </script>
 
 <style>
