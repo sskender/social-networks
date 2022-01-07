5aba438150384c4b4feeb06458f819a8e5b279b4
Sven Skender <sskender@pm.me>
2022-01-07 16:59:14 +0100

    Passport redirect to client URL

--- server/routes/v1/auth.route.js
+++ server/routes/v1/auth.route.js
@@ -9,7 +9,10 @@ router.get('/twitter', passport.authenticate('twitter'))
 router.get('/twitter/callback', passport.authenticate('twitter'), authController.loginCallback)
 
 router.get('/facebook', passport.authenticate('facebook', { scope: ['user_likes', 'email'] }))
-router.get('/facebook/callback', passport.authenticate('facebook'), authController.loginCallback)
+router.get('/facebook/callback', passport.authenticate('facebook', {
+  successRedirect: process.env.CLIENT_HOME_PAGE_URL,
+  failureRedirect: process.env.CLIENT_HOME_PAGE_URL
+}))
 
 router.get('/logout', authController.logout)
 
