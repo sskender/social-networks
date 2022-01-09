0a8c5cc7ea009f3aebcfd62fa3a383e8d66c4ee3
Sven Skender <sskender@pm.me>
2022-01-09 13:31:14 +0100

    Implement Twitter login on the backend

--- server/routes/v1/auth.route.js
+++ server/routes/v1/auth.route.js
@@ -6,7 +6,10 @@ const authController = require('../../controllers/auth.controller')
 const router = express.Router()
 
 router.get('/twitter', passport.authenticate('twitter'))
-router.get('/twitter/callback', passport.authenticate('twitter'), authController.loginCallback)
+router.get('/twitter/callback', passport.authenticate('twitter', {
+  successRedirect: process.env.CLIENT_HOME_PAGE_URL,
+  failureRedirect: process.env.CLIENT_HOME_PAGE_URL
+}))
 
 router.get('/facebook', passport.authenticate('facebook', { scope: ['user_likes', 'email'] }))
 router.get('/facebook/callback', passport.authenticate('facebook', {
